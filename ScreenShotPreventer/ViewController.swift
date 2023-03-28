5e4d4dbd7f1ac2126e55c8089c9bd5db1b77d5f7
ShoheiTakino <shohei0522penguin@gmail.com>
2023-03-28 19:17:43 +0900

    Initial Commit

---
+++ ScreenShotPreventer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenShotPreventer
+//
+//  Created by 滝野翔平 on 2023/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
